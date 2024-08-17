acd7a195ba75950497c784e108b47f22615708e7
Parwiz Hussaini <p.syas90@yahoo.com>
2024-08-17 15:03:45 +0000

    Set up structure of game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
+function runGame() {
+    
+}
+
+function checkAnswer() {
+    
+}
+
+function calculateCorrectAnswer() {
+    
+}
+
+function incrementScore() {
+    
+}
+
+function incrementWrongAnswer() {
+    
+}
+
+function displayAddistionQuestion() {
+    
+}
+
+function displaySubtractQuestion() {
+    
+}
+
+function displayMultiplayQuestion() {
+    
+}
 
